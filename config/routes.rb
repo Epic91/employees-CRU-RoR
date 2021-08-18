1587e2f41e4dd29a16b31ba3a8f9b06dc558bd10
Epic91 <hernandeze791@gmail.com>
2021-08-18 17:42:05 -0500

    used resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'employees/index'
-  get 'employees/new'
-  get 'employees/create'
-  get 'employees/show'
-  get 'employees/edit'
-  get 'employees/update'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :employees, only: [:index, :new, :create, :show, :edit, :update]
 end
