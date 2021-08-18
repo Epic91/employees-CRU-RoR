0a3efa85ae43eb29d39d09c075e7ddf284b57e73
Epic91 <hernandeze791@gmail.com>
2021-08-18 17:41:03 -0500

    generated employes controller and CRU methods

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,19 @@
+class EmployeesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'employees/index'
+  get 'employees/new'
+  get 'employees/create'
+  get 'employees/show'
+  get 'employees/edit'
+  get 'employees/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
